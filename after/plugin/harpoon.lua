115ac871c6eff620ca71f6cec5d7bce176b77c8d
Erico <medeiroserico23@gmail.com>
2024-07-14 03:20:54 -0300

    feat: fix issues and setup like in workspace

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,4 +2,5 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>r", mark.rm_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
