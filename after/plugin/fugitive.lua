fa47a19dee6158f9d53659e44438d7c574892ecb
Erico <medeiroserico23@gmail.com>
2024-08-16 00:39:26 -0300

    feat: add git keymaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,9 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gps", function ()
+    vim.cmd("Git push")
+end, {desc = "[G]it [P]u[S]h"})
+vim.keymap.set("n", "<leader>gpl",function ()
+    vim.cmd("Git pull")
+end, {desc="[G]it [P]u[L]l"})
+
+vim.keymap.set("n", "<leader>gco", ":Git checkout ", {desc = "[G]it [C]heck[O]ut"})
