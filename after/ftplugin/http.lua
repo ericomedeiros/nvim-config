e0c20245df621b8e4b0b093e88889bcb1f68cde8
Erico <medeiroserico23@gmail.com>
2024-08-29 02:44:58 -0300

    feat: making curls

--- after/ftplugin/http.lua
+++ after/ftplugin/http.lua
@@ -1,8 +1,28 @@
 vim.keymap.set({'n','v'}, '<leader>mr', function ()
-  local curLine = vim.api.nvim_get_current_line()
-  vim.print(curLine)
+  local curlCmd = "curl -X "
+  curlCmd = curlCmd .. vim.api.nvim_get_current_line() .. " -H \""
+  vim.print(curlCmd)
   local se = vim.fn.search('###','cn') - 1
-  vim.print(se)
-  local req = vim.api.nvim_buf_get_lines(0, (vim.fn.line('.') - 1), se, false)
-  vim.print(req)
+  local req = vim.api.nvim_buf_get_lines(0, (vim.fn.line('.')), se, false)
+  local inHeader = true
+  for key, value in ipairs(req) do
+    vim.print(value:match("%S"))
+    if value:match("%S") ~= nil then
+      curlCmd = curlCmd .. value
+      
+      if value == "}" then
+        curlCmd = curlCmd .. "'"
+        break
+      end
+    else
+      curlCmd = curlCmd:sub(1,-3) .. "\" -D '"
+      inHeader = false
+    end
+    if inHeader then
+      curlCmd = curlCmd .. ", "
+    end
+  end
+  vim.print(curlCmd)
+  vim.cmd("vnew")
+  vim.cmd(":terminal "..curlCmd)
 end, {desc = '[M]ake [R]equest'})
