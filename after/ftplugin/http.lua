06f258fafad4475696013c9ce4e300a33b4ee440
i505805 <erico.medeiros01@sap.com>
2024-08-30 00:29:00 -0300

    feat: working simple curl req

--- after/ftplugin/http.lua
+++ after/ftplugin/http.lua
@@ -1,6 +1,7 @@
 vim.keymap.set({'n','v'}, '<leader>mr', function ()
   local curlCmd = "curl -X "
-  curlCmd = curlCmd .. vim.api.nvim_get_current_line() .. " -H \""
+  curlCmd = curlCmd .. vim.api.nvim_get_current_line()
+  curlCmd = curlCmd:gsub("?", "\\?")
   vim.print(curlCmd)
   local se = vim.fn.search('###','cn') - 1
   local req = vim.api.nvim_buf_get_lines(0, (vim.fn.line('.')), se, false)
@@ -8,19 +9,20 @@ vim.keymap.set({'n','v'}, '<leader>mr', function ()
   for key, value in ipairs(req) do
     vim.print(value:match("%S"))
     if value:match("%S") ~= nil then
-      curlCmd = curlCmd .. value
+      if value == "{" then
+        curlCmd = curlCmd:sub(1,-3) .. " -D '"
+        inHeader = false
+      end
+      curlCmd = curlCmd  .. " -H '" .. value .. "'"
       
       if value == "}" then
         curlCmd = curlCmd .. "'"
         break
       end
-    else
-      curlCmd = curlCmd:sub(1,-3) .. "\" -D '"
-      inHeader = false
     end
-    if inHeader then
-      curlCmd = curlCmd .. ", "
-    end
+  end
+  if inHeader then
+    curlCmd = curlCmd:sub(1,-3) .. "'"
   end
   vim.print(curlCmd)
   vim.cmd("vnew")
